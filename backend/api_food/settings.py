77054d5bcb44527d2cf1bec4d82cf8af510be514
KhantsevDanil <khantsev.danil@gmail.com>
2021-12-13 21:03:07 +0300

    paginator test 1

--- backend/api_food/settings.py
+++ backend/api_food/settings.py
@@ -111,10 +111,10 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
-    'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE':
-        6,
+    'DEFAULT_PAGINATION_CLASS': 'api_food.paginator.CorePagination',
+    'PAGE_SIZE': 6,
+    'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S',
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 }
 DJOSER = {
     "HIDE_USERS": False,
